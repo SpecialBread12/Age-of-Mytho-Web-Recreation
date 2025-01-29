398cb43b31b7eea7c63f027c20c62aeadbda4300
SpecialBread12 <SpecialBread12@gmail.com>
2025-01-29 13:25:56 -0500

    création du header.php #8

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Site Web HTML</title>
+    <link rel="stylesheet" href="StyleCss/General.css" />
+    <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
+        integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc"
+        crossorigin="anonymous" />
+    <link rel="stylesheet" href="StyleCss/Index.css">
+    <script src="scripts/Compte à Rebour.js" defer></script>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <span class="navbar-toggle" id="js-navbar-toggle">
+                <!-- icône hamburger avec animation CSS -->
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </span>
+            <ul class="menu" id="js-menu">
+                <li><a href="index.html"><span class="fab fa-fort-awesome-alt"></span>Accueil</a></li>
+                <li><a href="Article_Index.html">Article<span class="fas fa-sort-down"></span></a>
+                    <ul>
+                        <li><a href="Article1.html">Age of Mythology</a></li>
+                        <li><a href="Article2.html">Age of Mythology EX:Titan</a></li>
+                        <li>
+                            <a href="Article3.html">Age of Mythology EX:Tale of the dragon</a>
+                        </li>
+                    </ul>
+                <li><a href="Mission.html">Mission</a></li>
+                <li><a href="Membre.html"><span class="fas fa-id-card"></span>Membre</a></li>
+                <li><a href="Contact.html"><span class="fas fa-user-secret"></span>Contact</a></li>
+                <li><a href="FAQ.html">FAQ</a></li>
+            </ul>
+        </nav>
+        <h1>Accueil</h1>
+    </header>
